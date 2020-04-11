5f528370678540c5e876a5f2ae7ef5b5cd937e8c
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2020-04-11 15:31:13 +0300

    second commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+    
+const Profile = () => {
+  return  <div className='content'>
+    <div>
+      <img src='https://dbijapkm3o6fj.cloudfront.net/resources/20611,1004,1,6,4,0,960,330/-3842-/20161013141806/image-gallery.jpeg'></img>  
+    </div>
+    <div>
+       ava + description 
+    </div>
+    <div>
+       My posts
+        <div>
+          New post
+        </div>
+        <div>
+           <div>
+             post 1
+           </div>
+           <div>
+             post 1
+           </div>
+        </div>
+    </div>
+  </div>
+}
+
+export default Profile;
